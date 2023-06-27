b41ab263b04063472eb87c6bff155eeb4c075f01
Lucas Kaique <lucaskaique743@gmail.com>
2023-06-27 20:43:43 -0300

    Adicionando novas fuções

--- functions_APE.py
+++ functions_APE.py
@@ -1,5 +1,6 @@
 import random
 
+#verifica se as celulas laterais a posição (i, j) está disponível.
 def verifica_adj_L(matriz, i, j):
         if (i+1) < 8 and matriz[i+1][j] == 1:
             return False
@@ -13,7 +14,8 @@ def verifica_adj_L(matriz, i, j):
             return False
         else: 
             return True
-        
+
+#verifica se as celulas diagonais a posição (i, j) está disponível.
 def verifica_adj_D(matriz, i, j):
         if (i+1) < 8 and (j+1) < 8 and matriz[i+1][j+1] == 1:
             return False
@@ -25,9 +27,10 @@ def verifica_adj_D(matriz, i, j):
             return False
         else: 
             return True
-        
-#PARÂMETROS: o tabuleiro e o número de navios
-def criar_tabuleiro(matriz,N):
+
+#gera o tabuleiro
+def gerar_tabuleiro(N):
+    matriz = [[0 for i in range(8)] for i in range(8)]
     cont = 0
     while True:
         x = random.randint(0,7)
@@ -45,6 +48,67 @@ def criar_tabuleiro(matriz,N):
             else:
                 matriz[i][j] = "A"
 
-            print(f"{matriz[i][j]:4}", end="")
-        print("")
     return matriz
+
+#exibe as frotas 
+def exibir_tabuleiro(jogador1, jogador2):
+    print()
+    print("Jogador 1")
+    print()
+    for i in range(8):
+        for j in range(8):
+            print(f"{jogador1[i][j]:4}", end="")
+        print("")
+
+    print()
+    print("Jogador 2")
+    print()
+
+    for i in range(8):
+        for j in range(8):
+            print(f"{jogador2[i][j]:4}", end="")
+        print("")
+    print()
+
+#exibe o tabuleiro para o jogo.
+def exibir_game(tab1, tab2):
+    print()
+    print("Jogador 1")
+    print()
+
+    for i in range(8):
+        for j in range(8):
+            print(f"{tab1[i][j]:4}", end="")
+        print("")
+
+    print()
+    print("Jogador 2")
+    print()
+
+    for i in range(8):
+        for j in range(8):
+            print(f"{tab2[i][j]:4}", end="")
+        print("")
+    print()
+
+#gera o menu interativo
+def menu(jog1, jog2):
+    print("""\
+    
+    Bem vindo a BATALHA NAVAL:
+
+    Menu:
+    Digite 1 para iniciar o Jogo:
+    Digite 2 para exibir as frotas:
+    Digite 3 para encerrar o Jogo:
+
+    """)
+        
+    menu = int(input("Digite o que deseja: "))
+    if menu == 1:
+        return True
+    elif menu == 2:
+       exibir_tabuleiro(jog1, jog2)
+    else:
+        return False
+    
--- main.py
+++ main.py
@@ -1,37 +1,111 @@
-import random
-import functions_APE
+from functions_APE import *
 
-matriz = [[0 for i in range(8)] for i in range(8)]
-N = int(input("Digite o numero de navios: "))
+while True:
+    N = int(input("Digite o numero de navios: (1 - 6)"))
+    if N <= 6 and N > 0:
+        break
+    else:
+        print("Insira um valor válido entre 1 e 6")
 
-tabuleiro_1 = str(functions_APE.criar_tabuleiro(matriz,N))
+jogador1 = gerar_tabuleiro(N)
+jogador2 = gerar_tabuleiro(N)
 
-file_player_1 = open('tabuleiros\jogador1.txt','w')
-file_player_1.write(tabuleiro_1)
-file_player_1.close()
+test = menu(jogador1, jogador2)
+while test:
+    if test:
+        event = 0
+        tab_1 = [["X" for i in range(8)] for i in range(8)]
+        tab_2 = [["X" for i in range(8)] for i in range(8)]
 
-file_player_2=open('tabuleiros\jogador1.txt','r')
-teste = file_player_2.read().replace("'","")
-teste2 = list(teste)
-print(type(teste2))
-print(len(teste2))
+        exibir_game(tab_1, tab_2)
 
-matriz_teste = [[0 for i in range(8)] for i in range(8)]
+        while True:
+            print()
+            print("Para acessar o menu digite: 8")
+            print()
+            print("Vez do jogador 1")
+            print()
 
-for  i in range(len(teste2)):
-    for  j in range(len(teste2)):
-        if teste2[i][j] == "A" :
-            matriz_teste[i][j] = "A"
-        elif teste2[i][j] == "N" :
-            matriz_teste[i][j] = "N"
+            linha = int(input("Digite a posição da linha"))
+            if linha == 8: 
+                menu(jogador1, jogador2)
+                break
+            coluna = int(input("Digite a posição da coluna"))
+
+            if linha < 8 and coluna < 8:
+                if jogador1[linha][coluna] == "N":
+                    tab_1[linha][coluna] = 'N'
+
+                    print("""
+
+                    #########   FOGO   ##########
+                    
+                    """)
+
+                    exibir_game(tab_1, tab_2)
+                else: 
+
+                    print("""
+
+                    #########   AGUA   ##########
+
+                    """)
+
+                    tab_1[linha][coluna] = 'A'
+                    exibir_game(tab_1, tab_2)
+
+            else:
+                print("Digite um valor válido")
+                print()
+                print("Perdeu a vez")
+                print()
+
+            print()
+            print("Para acessar o menu digite: 8")
+            print()
+            print("Vez do jogador 2")
+            print()
+
+            linha = int(input("Digite a posição da linha"))
+            if linha == 8: 
+                menu(jogador1, jogador2)
+                break
+            coluna = int(input("Digite a posição da coluna"))
+
+            if linha < 8 and coluna < 8:
+                if jogador1[linha][coluna] == "N":
+                    tab_2[linha][coluna] = 'N'
+
+                    print("""
+
+                    #########   FOGO   ##########
+
+                    """)
+
+                    exibir_game(tab_1, tab_2)
+                else: 
+
+                    print("""
+
+                    #########   AGUA   ##########
+
+                    """)
+
+                    tab_2[linha][coluna] = 'A'
+                    exibir_game(tab_1, tab_2)
+            else:
+                print("Digite um valor válido")
+                print()
+                print("Perdeu a vez")
+                print()
+    else:
+        print("Você encerrou o programa !!")
+
+
+
         
-for i in range(8):
-    for j in range(8):
-        print(f'{matriz_teste[i][j]:4}',end="")
-    print()
 
 
-file_player_2.close()
 
 
 
