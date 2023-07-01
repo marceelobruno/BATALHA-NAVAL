4a8abc8972d68a0df653cb14fc8ecffba8902c35
Marcelo Bruno <mbrunorp@gmail.com>
2023-07-01 14:15:21 -0300

    feat: add ref linhas/colunas

--- functions_APE.py
+++ functions_APE.py
@@ -1,4 +1,6 @@
-"""Este módulo contém todas as funções utilizadas no jogo Batalha Naval.
+"""Este módulo contém todas as funções utilizadas no jogo Batalha Naval
+desenvolvido para a disciplina Algoritmo e Programação Estruturada
+pelos alunos: Luiz Fernando, Lucas Kaique e Marcelo Bruno.
 
 Returns:
     Por conter todas as funções utilizadas na construção, salvamento e
@@ -7,6 +9,7 @@ Returns:
     de salvamento da partida que não há retorno.
 """
 
+# Bibliotecas utilizadas
 import os
 import pickle
 import random
@@ -27,16 +30,27 @@ def verifica_adj_L(matriz: list[list], i: int, j: int) -> bool:
         já estiver ocupada por um valor 1.
     """
 
-    if (i + 1) < 8 and matriz[i + 1][j] == 1:
+    # Exemplo de posição da matriz: [0][0], quando a linha e a coluna forem igual a 0.
+    # Verifica se há o número 1 abaixo da posição do exemplo informado acima.
+    if (i + 1) < 9 and matriz[i + 1][j] == 1:
         return False
-    elif (j + 1) < 8 and matriz[i][j + 1] == 1:
+
+    # Verifica se há o número 1 imediatamente ao lado direito do exemplo informado acima.
+    elif (j + 1) < 9 and matriz[i][j + 1] == 1:
         return False
-    elif (i - 1) >= 0 and matriz[i - 1][j] == 1:
+
+    # Verifica se há o número 1 imediatamente acima da posição do exemplo informado.
+    elif (i - 1) >= 1 and matriz[i - 1][j] == 1:
         return False
-    elif (j - 1) >= 0 and matriz[i][j - 1] == 1:
+
+    # Verifica se há o número 1 imediatamente ao lado esquerdo do exemplo informado.
+    elif (j - 1) >= 1 and matriz[i][j - 1] == 1:
         return False
+
+    # Verifica se há o número 1 na mesma posição do exemplo informado.
     elif matriz[i][j] == 1:
         return False
+
     else:
         return True
 
@@ -44,7 +58,6 @@ def verifica_adj_L(matriz: list[list], i: int, j: int) -> bool:
 def verifica_adj_D(matriz: list[list], i: int, j: int) -> bool:
     """Esta função verifica se há valores 1 nas diagonais de um
     outro valor 1 selecionado na matriz.
-
 
     Args:
         matriz (list[list]): Refere-se à matriz gerada.
@@ -56,44 +69,61 @@ def verifica_adj_D(matriz: list[list], i: int, j: int) -> bool:
         pelas diagonais. Caso contrário, retornará False.
     """
 
-    if (i + 1) < 8 and (j + 1) < 8 and matriz[i + 1][j + 1] == 1:
+    # Exemplo de posição da matriz: [0][0], quando a linha e a coluna forem igual a 0.
+    # Verifica se há o número 1 na diagonal direita, abaixo da posição do exemplo informado acima.
+    if (i + 1) < 9 and (j + 1) < 9 and matriz[i + 1][j + 1] == 1:
         return False
-    elif (i + 1) < 8 and (j - 1) >= 0 and matriz[i + 1][j - 1] == 1:
+
+    # Verifica se há o número 1 na diagonal esquerda, abaixo da posição do exemplo informado.
+    elif (i + 1) < 9 and (j - 1) >= 1 and matriz[i + 1][j - 1] == 1:
         return False
-    elif (i - 1) >= 0 and (j + 1) < 8 and matriz[i - 1][j + 1] == 1:
+
+    # Verifica se há o número 1 na diagonal direita, acima da posição do exemplo informado.
+    elif (i - 1) >= 1 and (j + 1) < 9 and matriz[i - 1][j + 1] == 1:
         return False
-    elif (i - 1) >= 0 and (j - 1) >= 0 and matriz[i - 1][j - 1] == 1:
+
+    # Verifica se há o número 1 na diagonal esquerda, acima da posição do exemplo informado.
+    elif (i - 1) >= 1 and (j - 1) >= 1 and matriz[i - 1][j - 1] == 1:
         return False
+
     else:
         return True
 
 
-def gerar_tabuleiro(num_navios):
+def gerar_tabuleiro(num_navios: int) -> list[list]:
     """Esta função gera o tabuleiro do jogo como uma matriz de elementos
-    1 e 0 e os transforma em "N" e "A". Esta função utiliza outras funções
-    criadas, como: verifica_adj_L() e verifica_adj_D().
+    1 e 0 e os transforma em `N` (navio) e `A` (água). Esta função utiliza
+    outras funções criadas, como: `verifica_adj_L()` e `verifica_adj_D()`.
 
     Args:
-        num_navios (int):  contém o número de navios selecionados.
+        num_navios (int): Número de navios da partida.
 
     Returns:
-        Retorna o tabuleiro gerado com os elementos "A" (água) e "N" (navio)
+        list[list]: Retorna o tabuleiro gerado com os elementos `A` e `N`.
     """
 
-    matriz = [[0 for i in range(8)] for i in range(8)]
+    matriz = [[0 for i in range(9)] for i in range(9)]
+
     cont = 0
     while True:
-        x = random.randint(0, 7)
-        y = random.randint(0, 7)
+        x = random.randint(1, 8)
+        y = random.randint(1, 8)
         if verifica_adj_L(matriz, x, y) and verifica_adj_D(matriz, x, y):
             matriz[x][y] = 1
             cont += 1
             if cont == num_navios:
                 break
 
-    for i in range(8):
-        for j in range(8):
-            if matriz[i][j] == 1:
+    # Vetor contendo as referências (A a H)
+    referencias_lin_col = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+    for i in range(9):
+        for j in range(9):
+            if i == 0:
+                matriz[i][j] = referencias_lin_col[j]
+            elif j == 0:
+                matriz[i][j] = referencias_lin_col[i * 1]
+            elif matriz[i][j] == 1:
                 matriz[i][j] = "N"
             else:
                 matriz[i][j] = "A"
@@ -102,11 +132,11 @@ def gerar_tabuleiro(num_navios):
 
 
 def exibir_tabuleiro(jogador1: list[list], jogador2: list[list]) -> list[list]:
-    """A função exibe as frotas como uma matriz em forma de tabuleiro para os usuários. 
+    """A função exibe as frotas como uma matriz em forma de tabuleiro para os usuários.
     Caso a exibição das frotas seja feita antes da partida iniciar, esta será encerrada.
 
     Args:
-        jogador1 (list[list]):Contém a frota do jogador 1.
+        jogador1 (list[list]): Contém a frota do jogador 1.
         jogador2 (list[list]): Contém a frota do jogador 2.
 
     Returns:
@@ -115,22 +145,24 @@ def exibir_tabuleiro(jogador1: list[list], jogador2: list[list]) -> list[list]:
 
     print("\nJogador 1")
     print()
-    for i in range(8):
-        for j in range(8):
-            print(f"{jogador1[i][j]:4}", end="")
+    for i in range(9):
+        for j in range(9):
+            print(f"{jogador1[i][j]:4}", end=" ")
         print("")
 
     print("\nJogador 2")
     print()
-    for i in range(8):
-        for j in range(8):
-            print(f"{jogador2[i][j]:4}", end="")
+    for i in range(9):
+        for j in range(9):
+            print(f"{jogador2[i][j]:4}", end=" ")
         print("")
     print()
 
 
 def exibir_game(tab1: list[list], tab2: list[list]) -> list[list]:
-    """Exibe para cada jogador um tabuleiro com as casas em forma de `X`.
+    """A função recebe uma matriz existente com todos os seus índices preenchidos pela
+    letra `X` e faz o tratamento adicionando para cada jogador um tabuleiro referenciado
+    a primeira linha e coluna pelas letras (A a H).
 
     Args:
         tab1 (list[list]): Tabuleiro do jogador 1
@@ -139,98 +171,116 @@ def exibir_game(tab1: list[list], tab2: list[list]) -> list[list]:
     Returns:
         list[list]: Os tabuleiros dos jogadores
     """
+    # Vetor contendo as referências (A a H)
+    referencias_lin_col = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
     print("\nJogador 1")
     print()
-
-    for i in range(8):
-        for j in range(8):
-            print(f"{tab1[i][j]:4}", end="")
+    for i in range(9):
+        for j in range(9):
+            if i == 0:
+                tab1[i][j] = referencias_lin_col[j]
+            elif j == 0:
+                tab1[i][j] = referencias_lin_col[i * 1]
+            print(f"{tab1[i][j]:4}", end=" ")
         print("")
 
     print("\nJogador 2")
     print()
-    for i in range(8):
-        for j in range(8):
-            print(f"{tab2[i][j]:4}", end="")
+    for i in range(9):
+        for j in range(9):
+            if i == 0:
+                tab2[i][j] = referencias_lin_col[j]
+            elif j == 0:
+                tab2[i][j] = referencias_lin_col[i * 1]
+            print(f"{tab2[i][j]:4}", end=" ")
         print("")
-    print()
 
 
-def salva_jogo(jog1: list[list], jog2: list[list], folderPath: str, tab1, tab2):
+def salva_jogo(jog1: list[list], jog2: list[list], folderPath: str,
+               tab1: list[list], tab2: list[list]):
     """Salva a partida em andamento possibilitando uma posterior continuação.
     A função baseai-se nos nomes fornecidos pelos jogadores e deve obedecer a ordem:
     `nome do 1° jogador` + `nome do 2° jogador`, uma vez que o diretório é salvo
     da seguinte forma: `Player1_Player2`.
 
     Args:
-        jog1 (list): Matriz contendo a partida do jogador 1
-        jog2 (list): Matriz contendo a partida do jogador 2
-        folderPath (str): Caminho onde o jogo será salvo
+        jog1 (list[list]): Matriz contendo a partida do jogador 1.
+        jog2 (list[list]): Matriz contendo a partida do jogador 2.
+        folderPath (str): Caminho onde o jogo será salvo.
+        tab1 (list[list]): Matriz contendo o tabuleiro da partida do jogador 1.
+        tab2 (list[list]): Matriz contendo o tabuleiro da partida do jogador 2.
     """
 
     # Transforma o parâmetro folderPath em um caminho relativo.
     caminho = fr'boards/{folderPath}'
 
-    # Cria o diretório caso o path informado não exista.
+    # Cria o diretório caso o caminho informado não exista.
     if not os.path.exists(caminho):
         os.makedirs(caminho)
 
-    # Se o diretório for criado na chamada da função ou já existir,
-    # salvará a partida no caminho informado.
+    # Se o diretório for criado na chamada da função ou já
+    # existir, salvará a partida no caminho informado.
     if os.path.exists(caminho):
-        with open(fr'{caminho}/player_1.pkl', 'wb') as output:
-            pickle.dump(jog1, output)
 
-        with open(fr'{caminho}/player_2.pkl', 'wb') as output:
-            pickle.dump(jog2, output)
+        # Salvando partida e o tabuleiro do jogador_1
+        with open(fr'{caminho}/player_1.pkl', 'wb') as plyr_1:
+            pickle.dump(jog1, plyr_1)
+
+        with open(fr'{caminho}/tab_1.pkl', 'wb') as tb_1:
+            pickle.dump(tab1, tb_1)
+
+        # Salvando partida e o tabuleiro do jogador_2
+        with open(fr'{caminho}/player_2.pkl', 'wb') as plyr_2:
+            pickle.dump(jog2, plyr_2)
+
+        with open(fr'{caminho}/tab_2.pkl', 'wb') as tb_2:
+            pickle.dump(tab2, tb_2)
 
 
-def carrega_jogo(jog1: list[list], jog2: list[list], folderPath: str, tab1, tab2) -> list[list]:
+def carrega_jogo(plyr_1: list[list], plyr_2: list[list], folderPath: str,
+                 tb_1: list[list], tb_2: list[list],) -> list[list]:
     """Carrega uma partida existente e que foi armazenada na memória.
-    A função baseai-se nos nomes fornecidos pelo jogador e deve obedecer a ordem:
-    `nome do 1° jogador` + `nome do 2° jogador`, uma vez que o diretório é salvo
-    da seguinte forma: `Player1_Player2`.
+    A função baseai-se nos nomes fornecidos pelos jogadores e deve
+    obedecer a ordem: `nome do 1° jogador` + `nome do 2° jogador`, uma vez
+    que o diretório é salvo da seguinte forma: `\Player1_Player2\`, sendo os
+    nomes dos jogadores separados por um `_`.
 
     Args:
-        jog1 (list): Matriz contendo a partida do jogador 1.
-        jog2 (list): Matriz contendo a partida do jogador 2.
+        plyr_1 (list[list]): Matriz contendo a partida do jogador 1.
+        plyr_2 (list[list]): Matriz contendo a partida do jogador 2.
         folderPath (str): Caminho onde o jogo será carregado.
+        tb_1 (list[list]): Matriz contendo o tabuleiro da partida do jogador 1.
+        tb_2 (list[list]): Matriz contendo o tabuleiro da partida do jogador 2.
     """
 
     # Transforma o parâmetro folderPath em um caminho relativo.
     caminho = fr'boards/{folderPath}'
 
-    # Verificando se o caminho informado para recupeação do jogo existe, caso
+    # Verifica se o caminho informado para recupeação do jogo existe, caso
     # contrário, o usuário terá que informar novamente os nomes dos jogadores.
     if not os.path.exists(caminho):
         print('Partida desconhecida. Tente novamente!')
 
     else:
-        # Carregando partida do jogador_1
-        with open(fr'{caminho}/player_1.pkl', 'rb') as player_1_file:
-            jog1 = pickle.load(player_1_file)
+        # Carregando partida e o tabuleiro do jogador_1
+        with open(fr'{caminho}/player_1.pkl', 'rb') as plyr_1:
+            plyr_1 = pickle.load(plyr_1)
 
-        # Exibe a partida do jogador_1
-        for i in range(len(jog1)):
-            for j in range(len(jog1)):
-                print(f'{jog1[i][j]:4}', end=" ")
-            print()
+        with open(fr'{caminho}/tab_1.pkl', 'rb') as tb_1:
+            tb_1 = pickle.load(tb_1)
 
-        print()
+        # Carregando partida e o tabuleiro do jogador_2
+        with open(fr'{caminho}/player_2.pkl', 'rb') as plyr_2:
+            plyr_2 = pickle.load(plyr_2)
 
-        # Carregando partida do jogador_2
-        with open(fr'{caminho}/player_2.pkl', 'rb') as player_2_file:
-            jog2 = pickle.load(player_2_file)
+        with open(fr'{caminho}/tab_2.pkl', 'rb') as tb_2:
+            tb_2 = pickle.load(tb_2)
 
-        # Exibe a partida do jogador_2
-        for i in range(len(jog2)):
-            for j in range(len(jog2)):
-                print(f'{jog2[i][j]:4}', end=" ")
-            print()
+        return exibir_game(tb_1, tb_2)
 
 
-def menu(jog1: list[list], jog2: list[list], folder_path: str) -> str:
+def menu(jog1: list[list], jog2: list[list], folder_path: str, tab1, tab2) -> str:
     """ Apresenta comandos básicos de um menu que podem ser selecionados
     ao digitar um número inteiro. Esta função utiliza-se de outras funções,
     como: `carrega_jogo()`,`exibir_tabuleiro()` e `salva_jogo()`.
@@ -256,6 +306,7 @@ Menu:
         return True
     elif menu == 2:
         carrega_jogo(jog1, jog2, folder_path, tab1, tab2)
+        return True
     elif menu == 3:
         exibir_tabuleiro(jog1, jog2)
     elif menu == 4:
@@ -269,4 +320,3 @@ Menu:
             return False
     else:
         return False
-
